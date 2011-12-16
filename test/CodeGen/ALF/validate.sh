26dcfa6f055c6fcb85743d09b849f22f75728f38
Benedikt Huber <benedikt.huber@gmail.com>
2011-12-16 15:39:21 +0100

    ALF: update testsuite to make validate.sh pass

--- test/CodeGen/ALF/validate.sh
+++ test/CodeGen/ALF/validate.sh
@@ -3,6 +3,6 @@ function die() {
     echo $1 >&2
     exit 1
 }
-(cd harness && bash run_tests) || die '*** Harness Testsuite failed'
-(cd mrtc    && IGNORE_VOLATILES=yes bash run_tests) || die '*** MRTC testsuite failed (standalone+css)'
-(cd mrtc    && ALF_STANDALONE=yes   bash run_tests) || die '*** MRTC testsuite failed (standalone+multipath)'
+(cd harness && bash run_tests) || die 'Harness Testsuite failed'
+(cd mrtc    && IGNORE_VOLATILES=yes bash run_tests) || die 'MRTC testsuite failed (standalone+css)'
+(cd mrtc    && ALF_STANDALONE=yes   bash run_tests) || die 'MRTC testsuite failed (standalone+multipath)'
