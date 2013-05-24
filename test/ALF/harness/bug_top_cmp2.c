024b80ed46e1693c4fbf7108563170d0099e50a3
Benedikt Huber <benedikt@vmars.tuwien.ac.at>
2013-05-24 14:00:55 +0200

    test/ALF: cleanup and update expectations

--- test/ALF/harness/bug_top_cmp2.c
+++ test/ALF/harness/bug_top_cmp2.c
@@ -1,5 +1,9 @@
 /* compare pointers from different frames */
-/* Expected: [0..1] */
+/* NOTE: as of now, it is an error in ALF to compare pointers
+   from different frames, so it is expected that this test fails
+   as long as the ALF translator does not replace the subtraction
+   by TOP */
+/* Expected (undefined result): [0..1] */
 int a;
 int main() {
     int b = 3;
